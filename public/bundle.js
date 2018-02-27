ef700287ead0b522f06b77769331ce452373e72f
Skarley Guerrero <skarleyguerrero@gmail.com>
2018-02-27 09:27:47 +0000

    Complete

---
+++ public/bundle.js
@@ -0,0 +1,108 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+
+
+var numArr = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+var suitArr = ["&#9827;","&#9824;","&#9830;","&#9829;"];
+
+var num = Math.floor(Math.random() * 12);
+var suit = Math.floor(Math.random() * 4);
+
+/*if (suit == 0 || suit == 1) {
+document.querySelector("#middle").style.color = "black";
+}
+if (suit == 2 || suit == 3) {
+document.querySelector("#middle").style.color = "#FF0000";
+}*/
+
+var numbers = document.querySelectorAll("span");
+for (var i = 0, max = numbers.length; i < max; i++) {
+    numbers[i].innerHTML = numArr[num];
+}
+
+document.querySelector("h1").innerHTML = suitArr[suit];
+
+function setColor (color) {
+    numbers = document.querySelectorAll("#color");
+    for (var i = 0, max = numbers.length; i < max; i++) {
+    numbers[i].classList.toggle(color);
+}}
+
+if (suit == 0 || suit == 1) {
+    setColor("black");
+    }
+else {
+    setColor("red");
+}
+
+
+
+/***/ })
+/******/ ]);
---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+var numArr = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+var suitArr = ["&#9827;","&#9824;","&#9830;","&#9829;"];
+
+var num = Math.floor(Math.random() * 12);
+var suit = Math.floor(Math.random() * 4);
+
+var numbers = document.querySelectorAll("span");
+for (var i = 0, max = numbers.length; i < max; i++) {
+    numbers[i].innerHTML = numArr[num];
+}
+
+document.querySelector("h1").innerHTML = suitArr[suit];
+
+function setColor (color) {
+    numbers = document.querySelectorAll("#color");
+    for (var i = 0, max = numbers.length; i < max; i++) {
+    numbers[i].classList.toggle(color);
+}}
+
+if (suit == 0 || suit == 1) {
+    setColor("black");
+}
+else {
+    setColor("red");
+}
+
